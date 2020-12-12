89ab72dbdc1d74ecf7403d1d7e70350e4c05bf17
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2020-12-12 19:48:22 +0600

    Dark Mode Added

---
+++ CRM-Software/resources/views/partials/superAdmin_sidebar.blade.php
@@ -0,0 +1,79 @@
+<div class="left-side-bar">
+    <div class="brand-logo">
+        <a href="index.html">
+            <img src="{{asset('assets/vendors/images/deskapp-logo.svg')}}" alt="" class="dark-logo">
+            <img src="{{asset('assets/vendors/images/deskapp-logo-white.svg')}}" alt="" class="light-logo">
+        </a>
+        <div class="close-sidebar" data-toggle="left-sidebar-close">
+            <i class="ion-close-round"></i>
+        </div>
+    </div>
+    <div class="menu-block customscroll">
+        <div class="sidebar-menu">
+            <ul id="accordion-menu">
+                
+                <li>
+                    <a href="{{route('superAdmin.index')}}" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-calendar1"></span><span class="mtext">Dashboard</span>
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Administrator</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="/supAdmin_home/supAdmin">Super Admin</a></li>
+                        <li><a href="/supAdmin_home/admin">Admin</a> |</li>
+                       
+                    </ul>
+                </li>
+               
+               
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Subscriber</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="/supAdmin_home/subscriber">Manage Subscriber</a></li>
+                        <li><a href="/supAdmin_home/feedbacks">Feedbacks</a> |</li>
+                        <li><a href="/supAdmin_home/verification">Verification</a> |</li>
+                       
+                    </ul>
+                </li>
+                <li>
+                    <a href="/supAdmin_home/package" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-calendar1"></span><span class="mtext">Package</span>
+                    </a>
+                </li>
+                <!-- <li>
+                    <a href="/supAdmin_home/meeting" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-calendar1"></span><span class="mtext">Meeting</span>
+                    </a>
+                </li> -->
+                <li class="dropdown">
+                    <a href="javascript:;" class="dropdown-toggle">
+                        <span class="micon dw dw-house-1"></span><span class="mtext">Notice</span>
+                    </a>
+                    <ul class="submenu">
+                        <li><a href="/supAdmin_home/meeting">Meeting</a></li>
+                        <li><a href="/supAdmin_home/feedbacks">Others</a> |</li>
+                        
+                       
+                    </ul>
+                </li>
+                <li>
+                    <a href="/supAdmin_home/template" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-calendar1"></span><span class="mtext">Template</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/supAdmin_home/financial" class="dropdown-toggle no-arrow">
+                        <span class="micon dw dw-calendar1"></span><span class="mtext">Financial Status</span>
+                    </a>
+                </li>
+              
+            </ul>
+        </div> 
+    </div>
+</div>
+<div class="mobile-menu-overlay"></div>
--- CRM-Software/resources/views/superAdmin/index.blade.php
+++ CRM-Software/resources/views/superAdmin/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 @include('partials.navbar')
-@include('partials.admin_sidebar')
+@include('partials.superAdmin_sidebar')
 <div class="main-container">
     <h1>afasdfsg</h1>
 </div>
