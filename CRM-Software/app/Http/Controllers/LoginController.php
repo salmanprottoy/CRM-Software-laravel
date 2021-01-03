ce384284bd2bcf91164871973a432f6c6ace7a46
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-01-03 11:28:10 +0600

    Update LoginController.php

--- CRM-Software/app/Http/Controllers/LoginController.php
+++ CRM-Software/app/Http/Controllers/LoginController.php
@@ -4,11 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
-class LoginController extends Controller
+class loginController extends Controller
 {
-    public function verify()
+    public function index()
     {
-        return view('Adminlogin.index');
+    	return view('login.index');
+    }
+    public function verify(Request $req)
+    {
+    	$user = DB::table('user')
+                    ->where('username', $req->username)
+                    ->where('password', $req->password)
+                    ->get();
+    	if(count($user)>0)
+    	{
+            $req->session()->put('username', $req->username);
+            $req->session()->put('id', $user[0]->id);
+            $req->session()->put('email', $user[0]->email);
+            $req->session()->put('phone', $user[0]->contactNumber);
+            $req->session()->put('designation', $user[0]->designation);
+    		return redirect()->route('markethome.index');
+    	}
+        else
+        {
+            return redirect('/login');
+        }
     }
 }
