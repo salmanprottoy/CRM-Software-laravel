ca48acdfa3d5abe48cdf1776fd8aace56182828c
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-01-06 11:36:54 +0600

    assignment partially dione

--- BlockChain-Validator_AdminPart/controller/user.js
+++ BlockChain-Validator_AdminPart/controller/user.js
@@ -22,6 +22,7 @@ router.get('/edit/:id', (req, res) => {
 		for (var i = 0; i < arrayOfObjects.length; i++) {
 			if (arrayOfObjects[i]['id'] == req.params.id) {
 				var user = {
+					pid: arrayOfObjects[i]['pid'],
 					name: arrayOfObjects[i]['name'],
 					email: arrayOfObjects[i]['email'],
 					mobile: arrayOfObjects[i]['mobile'],
@@ -31,10 +32,11 @@ router.get('/edit/:id', (req, res) => {
 					vote: arrayOfObjects[i]['vote'],
 				}
 				//console.log(user);
-
-				//console.log(arrayOfObjects[i]['id']);
+				console.log('sssssssssss');
+				console.log(arrayOfObjects[i]['id']);
 			}
 		}
+
 		res.render('user/edit', user);
 	})
 
@@ -47,19 +49,21 @@ router.post('/edit/:id', (req, res) => {
 		if (err) throw err
 
 		let arrayOfObjects = JSON.parse(data);
-		//console.log(arrayOfObjects.length);
+		//console.log(arrayOfObjects);
 
 
 		var uname = req.cookies['uname'];
-
+		console.log(uname);
 		for (var i = 0; i < arrayOfObjects.length; i++) {
 			if (arrayOfObjects[i]['id'] == req.params.id) {
 
 				arrayOfObjects[i]['vote'] = parseInt(req.body.vote) + 1;
 				arrayOfObjects[i]['prev_voter_id'] = uname;
+				//res.cookie('vote', arrayOfObjects[i]['vote']);
+
 				console.log(arrayOfObjects[i]['vote']);
 				console.log(arrayOfObjects[i]['prev_voter_id']);
-				break;
+
 			}
 
 		}
@@ -71,11 +75,77 @@ router.post('/edit/:id', (req, res) => {
 
 	})
 
+	// console.log('vote');
+	// console.log(req.body.vote);
 
-	res.redirect('/home/userlist');
+	userModel.getMaxID(req.params.id, function (result) {
+		console.log(result);
+		var user = result.id;
+		var total_vote = parseInt(req.body.vote) + 1;
+		var percentage = (total_vote / user) * 100;
+		console.log('percentage')
+		console.log(percentage);
+
+
+		var admin_update = {
+			id: req.body.id,
+			Name: req.body.name,
+			Mobile: req.body.mobile,
+			Email: req.body.email,
+			Gender: req.body.gender,
+			Address: req.body.address,
+			image: req.body.image
+		};
+		console.log('admin update');
+		console.log(admin_update);
+		if (percentage > 51) {
+			userModel.update(admin_update, function (status) {
+
+				if (status) {
+					res.redirect('/home/userlist');
+				} else {
+					res.redirect('/home');
+				}
+			});
+		}
+
+		// console.log('user');
+		// console.log(user);
+	});
+	// check();
+	// res.redirect('/home');
 
 	// res.redirect('/home/userlist');
 })
+
+
+// function check() {
+// 	fs.readFile('./assets/json/pending_transaction.json', 'utf-8', function (err, data) {
+// 		if (err) throw err
+
+// 		let arrayOfObjects = JSON.parse(data);
+// 		//console.log(arrayOfObjects);
+
+
+// 		// var uname = req.cookies['uname'];
+// 		console.log(uname);
+// 		for (var i = 0; i < arrayOfObjects.length; i++) {
+// 			if (arrayOfObjects[i]['id'] == req.params.id) {
+
+// 				arrayOfObjects[i]['vote'] = parseInt(req.body.vote) + 1;
+// 				arrayOfObjects[i]['prev_voter_id'] = uname;
+// 				//res.cookie('vote', arrayOfObjects[i]['vote']);
+
+// 				console.log(arrayOfObjects[i]['vote']);
+// 				console.log(arrayOfObjects[i]['prev_voter_id']);
+
+// 			}
+
+// 		}
+// 	console.log('Check');
+
+
+// }
 
 router.get('/delete/:id', (req, res) => {
 	fs.readFile('./assets/json/pending_transaction.json', 'utf-8', function (err, data) {
